bf8971067d9fb9c9c4c70e83707943b884f5f399
DMarzzucco <drrkermazyxokv@gmail.com>
2024-06-17 17:29:12 -0300

    frest

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { User } from "../ts/interfaces";
+
+const API = "http://localhost:3000/api";
+
+export const RegisterRequest = (user: User) => {
+    return axios.post(`${API}/Register`, user)
+        .then(response => {
+            console.log('Response', response.data)
+            return response.data;
+        })
+        .catch(error => {
+            console.error('There was an error', error)
+        })
+} 
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,24 +1,57 @@
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { RegisterRequest } from "../../api/api";
+import { User } from "../../ts/interfaces";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 function Register() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit } = useForm<User>();
+    const [registerFull, setRegisterFull] = useState<boolean>(false);
+    const [errorPass, setErrorPass] = useState<boolean>(false);
+
+    const onSubmit: SubmitHandler<User> = async (data: User) => {
+        console.log(data)
+        if (data.password.length < 6) {
+            setErrorPass(true)
+            setTimeout(() => setErrorPass(false), 1000);
+            return;
+        }
+        else {
+            try {
+                const res = await RegisterRequest(data);
+                setRegisterFull(true);
+                console.log(res)
+            }
+            catch (error) {
+                console.error("Submit Error", error)
+            }
+        }
+
+    }
     return (
         <>
             <section className="flex flex-col justify-center items-center w-full h-screen">
                 <h1>Register</h1>
-                <form onSubmit={handleSubmit((value) => {
-                    console.log(value);
-                })}
+                <form onSubmit={handleSubmit(onSubmit)}
                     className="flex flex-col justify-center items-center">
-                    <input type="text" {...register("userName", { required: true })}
+                    <input type="text" {...register("username", { required: true })}
                         placeholder="UserName "
                         className="text-white"
                     />
                     <input type="email" {...register("email", { required: true })}
                         placeholder="Email" />
-                    <input type="password" {...register("Password", { required: true })}
+                    <input type="password" {...register("password", { required: true })}
                         placeholder="Password" />
+                    {errorPass ?
+                        <div className="flex justify-center items-center w-full bg-red-500 text-white ">
+                            <h1>La contraseña debe ser de mas de 6 digitos</h1>
+                        </div> : null}
                     <button type="submit"> Sing Up</button>
                 </form>
+                {registerFull ?
+                    <div className="flex flex-col justify-center items-center">
+                        <h1>Registro con existo <br /> ahora puede iniciar seccion</h1>
+                        <Link to={"/login"} > Login </Link>
+                    </div> : null}
             </section>
         </>
     )
