d062581531115793c6b6757e7f2d68b0a8eb6151
darmarzz <123565631+DMarzzucco@users.noreply.github.com>
2024-11-24 10:58:22 -0300

    implement services Profile, refreshToken and LogOut in AuthController

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,18 +1,19 @@
-import { Body, Controller, Post, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiCookieAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiCookieAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from '../services/auth.service';
 import { DTOAuth } from '../dto/auth.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { CurrentUser } from '../decorators/current-user.decorator';
 import { UsersEntity } from '../../users/entities/users.entity';
+import { JwtAuthGuard, JwtRefreshAuthGuard } from '../guards';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly service: AuthService) { }
 
 
-    @ApiTags('Auth Login')
+    @ApiTags('Login')
     @UseGuards(LocalAuthGuard)
     @ApiBody({ type: DTOAuth })
     @ApiOperation({ summary: "Log a User - ROLE: PUBLIC_ACCESS" })
@@ -25,6 +26,31 @@ export class AuthController {
         @Res({ passthrough: true }) res: Response
     ) {
         return await this.service.generateToken(body, res)
+    }
+
+    @ApiTags("RefreshToken")
+    @UseGuards(JwtRefreshAuthGuard)
+    @ApiBody({ type: DTOAuth })
+    @Post("refresh")
+    public async RefreshToken(@CurrentUser() body: UsersEntity, @Res({ passthrough: true }) res: Response) {
+        return await this.service.generateToken(body, res);
+    }
+
+    @ApiTags("Profile")
+    @Get("profile")
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    public async GetProfile (@Req() req:Request){
+        return await this.service.profile(req);
+    }
+    @ApiTags("Log Out")
+    @Post("logout")
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
+    public async LogOut(@Req()req:Request, @Res() res:Response){
+        const userId= req.idUser;
+        await this.service.LogOut(userId, res)
+        return res.status(200).json({message:"Log Out successfully"})
     }
 
 
